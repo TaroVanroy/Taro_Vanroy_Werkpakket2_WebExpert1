0121fe27c5e1dddc7b836d2946c0f59617299318
TaroVanroy <Taro.Vanroy@student.pxl.be>
2023-12-22 21:45:24 +0100

    "22 December 21:45"

---
+++ Taro_Vanroy_Werkpakket2/src/components/HeaderComponent.vue
@@ -0,0 +1,99 @@
+<script>
+
+
+export default {
+  data() {
+    return {
+
+
+
+      navItems: [
+        { path: "/", name: "Home", display: true },
+        { path: "/Webshop", name: "Webshop", display: true },
+        { path: "/Cart", name: "Cart", display: true },
+        { path: "/Detail", name: "Detail", display: false },
+        { path: "/Checkout", name: "Checkout", display: false },
+        { path: "/Login", name: "Account", display: true },
+        { path: "/Bevestiging", name: "Bevestiging", display: false },
+      ],
+
+
+    };
+  },
+  computed: {
+    filteredNavItems() {
+      return this.navItems.filter(item => item.display);
+    },
+  },
+};
+</script>
+
+<template>
+  <nav class="navigatie">
+    <ul>
+      <li v-for="navItem in filteredNavItems" :key="navItem.path">
+        <router-link :to="navItem.path">{{ navItem.name }}</router-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+
+
+<style>
+
+
+
+.navigatie {
+  padding-top: 1rem;
+  text-align: right;
+  background-color: #718cfe;
+  height: 3.5rem;
+  width: 100%;
+
+}
+
+.navigatie li {
+  display: inline;
+  padding: 2rem;
+}
+
+.navigatie li a {
+  text-decoration: none;
+  color: white;
+  font-size: 20px;
+  font-family: inter, 'sans-serif';
+  font-weight: bold;
+}
+
+.navigatie button {
+  width: 7rem;
+  height: 2.5rem;
+  border-radius: 5px;
+  font-family: inter, 'sans-serif';
+  font-weight: bold;
+  color: #fff;
+  font-size: 16px;
+  border: 0;
+  background: rgb(188, 91, 237);
+  background: linear-gradient(270deg, rgba(188, 91, 237, 1) 0%, rgba(112, 141, 255, 1) 100%, rgba(2, 0, 36, 1) 202124%);
+}
+
+@media screen and (max-width: 600px)
+{
+  .navigatie
+  {
+    text-align: center;
+
+  }
+
+  .navigatie li a{
+   font-size: 16px;
+  }
+
+  .navigatie li {
+    padding: 1.5rem;
+  }
+}
+
+</style>
---
+++ Taro_Vanroy_Werkpakket2/src/components/PopularProductsComponent.vue
@@ -0,0 +1,116 @@
+<script>
+import { ref, onMounted } from 'vue';
+import { useShopStore } from '@/store/shop.js';
+
+export default {
+  setup() {
+    const shopStore = useShopStore();
+    const popularProducts = ref([]);
+
+    // Define getStockText within the setup function
+    const getStockText = (stock) => {
+      return stock > 0 ? `In Stock: ${stock}` : 'Out of Stock';
+    };
+
+    onMounted(async () => {
+      // Fetch popular products when the component is mounted
+      await shopStore.fetchProducts();
+      popularProducts.value = shopStore.popularProducts;
+    });
+
+      return {
+      popularProducts,
+      getStockText,
+      "titel1": "Popular Products",
+        "knop": "Add to cart"
+    };
+
+  },
+  methods: {
+    addToCart(product)
+    {
+      useShopStore().addToCart(product);
+    }
+  },
+};
+</script>
+
+<template>
+  <div>
+    <h1>{{ titel1 }}</h1>
+    <ul class="product-list">
+      <li v-for="product in popularProducts" :key="product.id" class="product-item">
+        <div class="product-info">
+          <router-link :to="{ name: 'product-detail', params: { id: product.id } }">
+            <img :src="product.image" alt="Product Image" class="product-image" /></router-link>
+          <div class="product-details">
+            <p>{{ product.name }}</p>
+            <p class="stock">{{ getStockText(product.stock) }}</p>
+            <button @click="addToCart(product)" class="add-to-cart-button">{{ knop }}</button>
+          </div>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style>
+
+.product-list {
+  list-style: none;
+  padding: 0;
+  display: flex;
+  justify-content: space-evenly;
+  margin-top: 6rem;
+}
+
+.product-item {
+  margin-right: 10px;
+}
+
+.product-info {
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  text-align: center;
+}
+
+.product-image {
+  width: 300px;
+  height: 300px;
+  border: 5px solid #B95CED;
+  border-radius: 5px;
+  padding: 15px;
+}
+
+.product-details {
+  max-width: 150px;
+  padding-top: 1.5rem;
+  color: #718CFE;
+  font-weight: bold;
+  font-size: 20px;
+}
+
+.stock
+{
+  padding-top: 1rem;
+}
+
+@media screen and (max-width: 600px) {
+  .product-list {
+    list-style: none;
+   margin-left: 50%;
+    margin-right: 50%;
+    display: flex;
+    justify-content: space-evenly;
+    margin-top: 6rem;
+    flex-direction: column;
+
+  }
+
+  .product-item {
+  }
+
+}
+
+</style>
